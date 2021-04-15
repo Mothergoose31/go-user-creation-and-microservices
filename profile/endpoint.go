8f19379b7fc7abfa6bf226ac798da034eac15e1b
Mothergoose31 <JosephAlvarengaBeech@protonmail.com>
2021-04-14 19:03:55 -0700

    fix typo for endpoint

--- profile/endpoint.go
+++ profile/endpoint.go
@@ -1,23 +1,25 @@
 package account
 
-import(
+import (
 	"context"
 
 	"github.com/go-kit/kit/endpoint"
 )
 
-type endpoints struct {
+type Endpoints struct {
 	CreateUser endpoint.Endpoint
-	GetUser endpoint.Endpoint
+	GetUser    endpoint.Endpoint
 }
-func MakeEndpoints(s Service)Endpoints{
-	return endpoints{
-		CreateUser:MakeCreateUserEndpoint(s),
-		GetUser:MakeGetUserEndpoint(s)
+
+func MakeEndpoints(s Service) Endpoints {
+	return Endpoints{
+		CreateUser: makeCreateUserEndpoint(s),
+		GetUser:    makeGetUserEndpoint(s),
 	}
 }
-fun MakeCreateUserEndpoint(s Service) endpoint.Endpoint{
-	return func(ctx context.Context,request (interface{})interface{},error){
+
+func makeCreateUserEndpoint(s Service) endpoint.Endpoint {
+	return func(ctx context.Context, request interface{}) (interface{}, error) {
 		req := request.(CreateUserRequest)
 		ok, err := s.CreateUser(ctx, req.Email, req.Password)
 		return CreateUserResponse{Ok: ok}, err
